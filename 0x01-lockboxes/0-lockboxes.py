276fb1eb7a77ed9a1f23e42a0ea295f476e918a8
starkjoy <residualcode6@gmail.com>
2023-10-03 16:02:46 +0000

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ Function to determine if all boxes can be opened """
 
+
 def canUnlockAll(boxes):
-    """ 
+    """
     Actual implementation of the function
 
     Arguments:
