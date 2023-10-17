09870f8e006758b532280378e22bbe3a692d6bbf
starkjoy <residualcode6@gmail.com>
2023-10-17 18:51:39 +0000

    Log Parse

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-
 import sys
 import signal
+
 
 # Initialize variables to store statistics
 file_size = 0
 status_code_counts = {}
 line_count = 0
+
 
 # Function to print statistics
 def print_statistics():
@@ -16,12 +17,15 @@ def print_statistics():
     for status_code in sorted(status_code_counts.keys()):
         print(f"{status_code}: {status_code_counts[status_code]}")
 
+
 # Handle SIGINT (Ctrl+C) to print statistics and continue
 def signal_handler(sig, frame):
     print_statistics()
     sys.exit(0)
 
+
 signal.signal(signal.SIGINT, signal_handler)
+
 
 try:
     for line in sys.stdin:
@@ -36,7 +40,8 @@ try:
             if status_code.isdigit():
                 status_code = int(status_code)
                 if status_code in (200, 301, 400, 401, 403, 404, 405, 500):
-                    status_code_counts[status_code] = status_code_counts.get(status_code, 0) + 1
+                    status_code_counts[status_code] = status_code_counts.get(
+                            status_code, 0) + 1
 
             line_count += 1
 
