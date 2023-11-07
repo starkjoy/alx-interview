0124721f377ba9775f1397881d6566a91820261e
starkjoy <residualcode6@gmail.com>
2023-11-07 16:09:23 +0000

    Done

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,45 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+// Define the Star Wars API base URL
+const apiUrl = 'https://swapi-api.alx-tools.com/api';
+
+// Function to fetch characters from a specific movie
+function getCharactersInMovie(movieId) {
+  const filmUrl = `${apiUrl}/films/${movieId}/`;
+  
+  request(filmUrl, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const filmData = JSON.parse(body);
+      const characters = filmData.characters;
+
+      if (characters.length === 0) {
+        console.log('No characters found for this movie.');
+      } else {
+        console.log(`Characters in the movie "${filmData.title}":`);
+        characters.forEach((characterUrl, index) => {
+          request(characterUrl, (error, response, characterBody) => {
+            if (!error && response.statusCode === 200) {
+              const characterData = JSON.parse(characterBody);
+              console.log(`${index + 1}. ${characterData.name}`);
+            } else {
+              console.error(`Error fetching character data: ${error}`);
+            }
+          });
+        });
+      }
+    } else {
+      console.error(`Error fetching movie data: ${error}`);
+    }
+  });
+}
+
+// Check if a movie ID is provided as a command-line argument
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.log('Please provide a movie ID as a command-line argument.');
+} else {
+  getCharactersInMovie(movieId);
+}
