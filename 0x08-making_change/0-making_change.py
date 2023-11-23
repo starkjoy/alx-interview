7458e3f6247719b87cef1016715a4e6ec93398f6
starkjoy <residualcode6@gmail.com>
2023-11-23 21:06:42 +0000

    Correct

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
-This module contains a function to determine the fewest number of coins needed to meet a given amount total.
+Function to determine the fewest number of coins needed to match an amount
 """
+
 
 def makeChange(coins, total):
     """
     Determine the fewest number of coins needed to meet the given total.
 
     Parameters:
-        coins (list of int): A list of integers representing the values of available coins.
+        coins (list of int): A list of available coins.
         total (int): The target total amount.
 
     Returns:
@@ -27,10 +28,3 @@ def makeChange(coins, total):
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
-
-# Example usage:
-if __name__ == "__main__":
-    coins = [1, 2, 5]
-    total = 11
-    result = makeChange(coins, total)
-    print(result)  # Output: 3 (11 = 5 + 5 + 1)
