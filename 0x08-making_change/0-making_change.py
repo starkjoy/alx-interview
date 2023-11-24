3631c3373c092e962c362bcef3744d4db2b2d5ac
starkjoy <residualcode6@gmail.com>
2023-11-24 08:32:25 +0000

    efficient

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -20,11 +20,20 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
+    smallest_amounts = {coin: float('inf') for coin in coins}
+    for coin in coins:
+        smallest_amounts[coin] = coin
+
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+    for amount in range(1, total + 1):
+        if amount in smallest_amounts:
+            dp[amount] = 1
+        else:
+            for coin in coins:
+                if amount >= coin:
+                    dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
+
