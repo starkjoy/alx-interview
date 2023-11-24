a69a4f1e416824734b0fcd643605353661dda9a1
starkjoy <residualcode6@gmail.com>
2023-11-24 08:29:13 +0000

    Return

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -17,20 +17,14 @@ def makeChange(coins, total):
              If total is 0 or less, return 0.
              If total cannot be met by any number of coins, return -1.
     """
-
-    if len(coins) <= 0 or total <= 0:
+    if total <= 0:
         return 0
-    
-    coins.sort()
-    
-    count = 0
-    
-    index = len(coins) - 1
-    
-    while total > 0 and index >= 0:
-        if coins[index] <= total:
-            count += total // coins[index]
-            total %= coins[index]
-        index -= 1
-    
-    return count
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
